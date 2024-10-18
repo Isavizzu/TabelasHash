2160b11f7a76be7cb4beac86bb058ba02a58595f
VizzuIsabelle <melovizzu.isabelle@gmail.com>
2024-10-18 11:56:31 -0300

    adicionando os métodos

--- TabelasHash/src/TabelaHash.java
+++ TabelasHash/src/TabelaHash.java
@@ -1,2 +1,12 @@
-public class TabelaHash {
+public abstract class TabelaHash {
+
+    public abstract void calcularHash(int number);
+
+    public void inserir(int number){
+
+    }
+
+//Talvez não precise do metodo de remover
+
+    public abstract void buscar(int number);
 }
