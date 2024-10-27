d2aac67ea85a5546545eee509c49898d94f2af11
Maria Fernanda <162508333+Mafezuda@users.noreply.github.com>
2024-10-27 00:30:52 -0300

    Atualizar o TabelaHash_2.java

--- TabelasHash/src/TabelaHash_2.java
+++ TabelasHash/src/TabelaHash_2.java
@@ -3,3 +3,12 @@ public class TabelaHash_2 extends TabelaHash {
     public TabelaHash_2(int size) {
         super(size);
     }
+
+    public int calculeHash(String chave) {
+        int valor = 0;
+        for (int i = 0; i < chave.length(); i++) {
+            valor = (valor << 5) - valor + chave.charAt(i);
+        }
+        return Math.abs(valor % super.getSize()); // Garante índice dentro dos limites
+    }
+}
